6bf2bf1ead103624742a773b93ac48da624045cd
Abhinav Tyagi <abhinavtyagi275@gmail.com>
2026-03-02 21:09:48 +0530

    Connected frontend to railway backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://expresit-production.up.railway.app", {
+  transports: ["websocket"],
+});
 
 function App() {
   const [name, setName] = useState("");
